814e7d237746ce86d6c41d3f40476dd9aea2dd19
노영선 <dudtjs303@yonsei.ac.kr>
2023-06-29 15:38:39 +0900

    화면 수정

--- src/main/front/src/component/Main/MainTablet.js
+++ src/main/front/src/component/Main/MainTablet.js
@@ -33,7 +33,7 @@ const MainTablet = (props) => {
                         <img src='/img/search.png' alt='검색' />
                     </button>
                 </div>
-                <div style={{textAlign: 'center'}}><Link to='/GetMySentiment'><button className={style.goSentiment}>내 글 감정분석 하기</button></Link></div>
+                <div style={{textAlign: 'center'}}><Link to='/GetMySentiment'><div className={style.goSentiment}>내 글 감정분석 하기</div></Link></div>
             </div>
         </div>
     );
